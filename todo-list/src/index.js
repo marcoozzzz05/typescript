45b0994dd711d3ec8b2069d4ad5f4f89bbc1737f
marcoozzzz05 <andronico.marco05@gmail.com>
2025-03-30 19:16:21 +0200

    add function

--- todo-list/src/index.js
+++ todo-list/src/index.js
@@ -1,3 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const todos = [];
+function addTodo(title) {
+    const newTodo = {
+        id: Date.now(),
+        title: title,
+        completed: false,
+    };
+    todos.push(newTodo);
+}
+addTodo("Comprare il latte");
+addTodo("Andare in palestra");
+console.log(todos);
--- todo-list/src/index.ts
+++ todo-list/src/index.ts
@@ -1,3 +1,16 @@
 import {Todo} from  "./types"
 
 const todos: Todo[] = [];
+
+function addTodo(title: string): void {
+    const newTodo: Todo = {
+      id: Date.now(), 
+      title: title,
+      completed: false,
+    };
+
+    todos.push(newTodo);
+}
+  
+addTodo("Titolo");  
+console.log(todos); 
